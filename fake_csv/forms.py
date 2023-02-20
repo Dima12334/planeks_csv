aa6fe58760bcdf89af1469b1a6478f43b9f13b88
Dima12334 <podolinnyjdima2@gmail.com>
2023-02-20 13:10:47 +0200

    Release version

--- fake_csv/forms.py
+++ fake_csv/forms.py
@@ -2,7 +2,6 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Field
 from django import forms
 from django.forms import modelformset_factory
-
 from .models import Schema, Column
 
 
@@ -11,16 +10,16 @@ class SchemaForm(forms.ModelForm):
 
     class Meta:
         model = Schema
-        fields = ('name', 'column_separator', 'string_character')
+        fields = ("name", "column_separator", "string_character")
 
     def __init__(self, *args, **kwargs):
         super(SchemaForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.label_class = 'd-flex justify-content-left'
+        self.helper.label_class = "d-flex justify-content-left"
         self.helper.layout = Layout(
-            Field('name', css_class='w-50'),
-            Field('column_separator', css_class='w-50'),
-            Field('string_character', css_class='w-50')
+            Field("name", css_class="w-50"),
+            Field("column_separator", css_class="w-50"),
+            Field("string_character", css_class="w-50"),
         )
 
 
@@ -29,12 +28,14 @@ class ColumnForm(forms.ModelForm):
 
     class Meta:
         model = Column
-        fields = ('name', 'type', 'from_value', 'to_value', 'order')
+        fields = ("name", "type", "from_value", "to_value", "order")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['from_value'].disabled = False
-        self.fields['to_value'].disabled = False
+        self.fields["from_value"].disabled = False
+        self.fields["to_value"].disabled = False
+        self.fields["from_value"].label = "Min Integer"
+        self.fields["to_value"].label = "Max Integer"
 
 
 ColumnFormSet = modelformset_factory(Column, form=ColumnForm, extra=0)
